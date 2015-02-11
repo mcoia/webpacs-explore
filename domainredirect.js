56c9a7a68a4fe06755834658b77eb40d05f9e68d
Megan Durham <megan@mobiusconsortium.org>
2015-02-11 17:04:58 -0600

    add domain redirect to test

---
+++ domainredirect.js
@@ -0,0 +1,25 @@
+var href = window.location.href
+var org = ".org"
+var domainLength = href.indexOf(org) + org.length
+var notDomain = href.substring(domainLength)
+var explore = 'http://explore.searchmobius.org'
+if (notDomain == "/") {
+var redirectUrl = explore + "/search~S0&redirected"
+}else {
+var r = /\W$/g
+if (r.test(notDomain) == true){
+notDomain = notDomain.slice(0,-1);
+}
+redirectUrl = explore + notDomain + "&redirected"
+}
+window.location = redirectUrl;
+if(href.indexOf("slrlc.org") != -1 ){
+var oldDomain = "slrlc.org"
+window.location = redirectUrl;
+}else{}
+if (href.indexOf("&redirected") != -1 ) {
+var redirectMessage = "<p>You've been redirected from slrlc.org. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://explore.searchmobius.org'>http://explore.searchmobius.org</a>.</p>"
+$(document).ready(function () {
+$('#alertMessage').html(redirectMessage);
+});
+}
