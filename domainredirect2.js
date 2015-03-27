5258a0510bc9615f536a698276be39a5c296ad4a
Megan Durham <megan@mobiusconsortium.org>
2015-03-27 16:34:06 -0500

    update domain redirect and wam proxy

--- domainredirect2.js
+++ domainredirect2.js
@@ -3,11 +3,11 @@ var spliturl = href.split(".org"); //splits url into array with beginning and sc
 var explore = 'http://explore.searchmobius.org' //new domain name
 var scope = spliturl[1].replace(/\/$/, ""); //gets scope information and removes '/' at the end
 
-if (spliturl[0] <= "http://slrlc.org"){ //if href matches unscoped url
+if (spliturl[0].indexOf("slrlc") != -1 || scope.indexOf(" ") != -1){ //if href matches unscoped url
   var redirectNoscope = explore + "/search~S0&redirected" //new url with no scope
   window.location = redirectNoscope; //add new url to window
-}else{}
-if (spliturl[1].indexOf(scope) == 0){ //if there is scope information
+}else {}
+ if (spliturl[0].indexOf("slrlc") !=-1 && spliturl[1].indexOf("/") != -1){ //if there is scope information
   var redirectWscope = explore + scope + "&redirected" //new url with scope information and redirect
   window.location = redirectWscope;
 }
@@ -20,7 +20,7 @@ if (scope == undefined || scope == 0){ //scope has something or nothing
    
 if (href.indexOf("&redirected") != -1 ) {
     
-    var redirectMessage = "<p>You've been redirected from" + href + " . Starting on May 1, 2015, those addresses will no longer work.<br>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
+    var redirectMessage = "<p>The domain name slrlc.org is being retired.<br>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
 
     $(document).ready(function () {
         $('#alertMessage').html(redirectMessage);
